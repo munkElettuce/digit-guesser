56b6ebf00c44b2988cc486d09f68ebff52d15566
Dhanush S <dhanushs3366@gmail.com>
2023-10-12 20:42:08 +0530

    moved some dataset created images from client side (organised my mess that is all)

--- utils/getNoOfImages.js
+++ utils/getNoOfImages.js
@@ -23,21 +23,33 @@
 
 // module.exports=countImagesInDirectory
 
-const readdirp = require('readdirp');
+const fs = require('fs');
 const path = require('path');
 
-async function countImagesInDirectory(directoryPath) {
+function countImagesInDirectory(directoryPath) {
   // Array of valid image file extensions
   const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.webp'];
 
   // Initialize a variable to count the number of images
   let imageCount = 0;
 
-  for await (const entry of readdirp(directoryPath, { fileFilter: ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', '*.svg', '*.webp'] })) {
-    imageCount++;
+  // Create a generator function to iterate over the files in the directory
+  function* generateFiles(directoryPath) {
+    const files = fs.readdirSync(directoryPath);
+    for (const file of files) {
+      yield path.join(directoryPath, file);
+    }
+  }
+
+  // Iterate over the files in the generator function and count the number of images
+  for (const file of generateFiles(directoryPath)) {
+    const ext = path.extname(file).toLowerCase();
+    if (imageExtensions.includes(ext)) {
+      imageCount++;
+    }
   }
 
   return imageCount;
 }
 
-module.exports = countImagesInDirectory;
+module.exports=countImagesInDirectory;
